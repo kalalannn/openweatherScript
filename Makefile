1493d933f36c58992e87178be2d54f13f000106d
kalalannn <39317136+kalalannn@users.noreply.github.com>
2019-02-13 14:40:14 +0100

    Add files via upload
    1.0 version

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	python3 proj.py $(api_key) $(city)
---
+++ proj.py
@@ -0,0 +1,27 @@
+import socket, ast, sys
+appid = sys.argv[1]; city = sys.argv[2]
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('api.openweathermap.org', 80))
+sock.send(bytes('GET /data/2.5/weather?q={0}&appid={1}\n'.format(city, appid), 'utf-8'))
+data = ast.literal_eval(sock.recv(1024).decode('utf-8'))
+sock.close()
+if data['cod'] == 200:
+    print(
+            data['name'] + '\n' +
+            data['weather'][0]['description'] + '\n' +
+            'temp: '         + str(round(float(data['main']['temp']) - 273.15, 2)) + u'\N{DEGREE SIGN}' + 'C\n' +
+            'humidity: '     + str(data['main']['humidity']) + '%\n'
+            'preasure: '     + str(data['main']['pressure']) + 'hPa'
+            )
+    try:
+        print(
+                'wind-speed: '   + str(data['wind']['speed'])    + 'km/h\n'
+                'wind-deg: '     + str(data['wind']['deg'])
+                )
+    except KeyError:
+        print(
+                'wind-speed: 0km/h\n' +
+                'wind-deg: -'
+                )
+else :
+    print(data['message'])
