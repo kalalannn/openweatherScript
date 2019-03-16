92d32deffd20dbf52720c45376ef5b9404937d5c
kalalannn <xvorob00@stud.fit.vutbr.cz>
2019-03-16 14:11:38 +0100

    final

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# openweatherScript
+# OpenWeatherApiScript
+* Author: Nikolaj Vorobiev
+* E-mail: xvorob00@stud.fit.vutbr.cz
+Using this script you will get the actual weather.
+
+# Using
+bash```make run api_key=<API_KEY> city=<CITY>```
+
+# Output format
++ City
++ Weather discription
++ Temperature
++ Humidity
++ Pleassure
++ Wind-speed
++ Wind-deg
--- proj.py
+++ proj.py
@@ -1,10 +1,20 @@
+# OpenWeatherApiScript
+# Author: Nikolaj Vorobib
+# Date: 20.02
+
 import socket, ast, sys
 appid = sys.argv[1]; city = sys.argv[2]
+# creating a new socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Connecting
 sock.connect(('api.openweathermap.org', 80))
+# Sending get request
 sock.send(bytes('GET /data/2.5/weather?q={0}&appid={1}\n'.format(city, appid), 'utf-8'))
+# Decoding response
 data = ast.literal_eval(sock.recv(1024).decode('utf-8'))
+# Closing socket
 sock.close()
+# Output
 if data['cod'] == 200:
     print(
             data['name'] + '\n' +
